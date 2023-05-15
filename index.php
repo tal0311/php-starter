4175ce47df87077c5a3b6de9f4325f1892e13ce1
tal0311 <tal.amit0311@gmail.com>
2023-05-16 00:11:17 +0300

    /

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
 <?php
-$uri = $_SERVER['REQUEST_URI'];
 
-var_dump($uri);
+
+require_once 'util.service.php';
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+dd($uri);
+
+$routes = [
+ '/' => 'index.php',
+ '/about' => 'about.php',
+ '/contact' => 'contact.php',
+ '/user' => 'user.php',
+ '/activities' => 'activities.php'
+];
+
+if (array_key_exists($uri, $routes)) {
+ require_once $routes[$uri];
+} else {
+ require_once '404.php';
+}
